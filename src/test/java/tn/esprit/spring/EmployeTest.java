905bb6adce8379e95e087b3fe0f569e6f53b3a1a
houdabettayeb <47000059+houdabettayeb@users.noreply.github.com>
2021-11-15 00:35:14 +0100

    Update EmployeTest.java

--- src/test/java/tn/esprit/spring/EmployeTest.java
+++ src/test/java/tn/esprit/spring/EmployeTest.java
@@ -94,7 +94,7 @@ Role.CHEF_DEPARTEMENT);
         employeService.affecterContratAEmploye(contrat.getReference(), employe.getId());
         float salaire1=0 ;
         float salaire = employeService.getSalaireByEmployeIdJPQL(employe.getId());
-       // Assert.assertNotEquals(salaire, salaire1);
+        Assert.assertNotEquals(salaire, salaire1);
         l.info("l'employé d'id: " + employe.getId() + " a un salaire de " + salaire);
     }
 
@@ -115,7 +115,7 @@ Role.CHEF_DEPARTEMENT);
     
    public void DeleteContrat() {
         Date date = new Date();
-       // Contrat contrat = new Contrat(date, "CDD", 1000);
+        Contrat contrat = new Contrat(date, "CDD", 1000);
         employeService.ajouterContrat(contrat);
         boolean existBeforeDelete = contratRepository.findById(contrat.getReference()).isPresent();
         employeService.deleteContratById(contrat.getReference());
@@ -132,7 +132,7 @@ Role.CHEF_DEPARTEMENT);
         boolean existBeforeDelete = employeRepository.findById(employe.getId()).isPresent();
         Assert.assertTrue(existBeforeDelete);
         employeService.deleteEmployeById(employe.getId());
-       // boolean existAfterDelete = employeRepository.findById(employe.getId()).isPresent();
+        boolean existAfterDelete = employeRepository.findById(employe.getId()).isPresent();
         Assert.assertFalse(existAfterDelete);
         l.info("L'employé est supprimé");
     }
