f81f4b54cb151ffd00793d28b46441f00ac95eaf
meyssensnene <meyssen.fekihhssinesnene@esprit.tn>
2021-11-17 16:25:30 +0100

    commit6

--- src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import tn.esprit.spring.ContratTest;
+
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.repository.ContratRepository;
 
